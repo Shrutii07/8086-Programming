e2f91d35e9b3adcc168edc54c666849c90da0a9e
Shruti Murarka <shrutimurarka5@gmail.com>
2021-09-19 22:10:04 +0530

    Update area.asm

--- Lab/6.area_of_rectangle/area.asm
+++ Lab/6.area_of_rectangle/area.asm
@@ -1,6 +1,8 @@
+;fsd
+
 Data Segment 
     msg1 db "Enter the Length : $"
-    msg2 db 13, 10, "Enter the Length : $"
+    msg2 db 13, 10, "Enter the Breadth : $"
     space db 13, 10, "Area is : $"
     length db ?
     breadth db ?
@@ -69,3 +71,50 @@ Start: MOV AX, Data
        INT 21h
 Code Ends
      End Start
+     
+     
+     ;ssd
+     
+ .model small
+.data
+    arr1 db 12,1,14,15,22 
+    cnt db 5
+    msg1 db 13,10,'Enter the length: $',13,10  ;0dh,0ah
+    msg2 db 13,10,'Enter the breadth: $',13,10     
+    msg3 db 13,10,'The area of the rectangle is: $',13, 10
+    msg4 db 13,10,'21$', 13,10   
+.code
+.startup   
+start:   
+    mov dx,offset msg1
+    mov ah,9
+    int 21h    
+    mov ah, 1h
+    int 21h                    
+    sub al, 30h                    
+    mov bl, al           
+    mov dx,offset msg2
+    mov ah,9
+    int 21h      
+    mov ah, 1h
+    int 21h       
+    sub al, 30h    
+    mul bl     
+    mov bl, al    
+    mov dx,offset msg3
+    mov ah,9
+    int 21h  
+    mov al, bl   
+    mov ah, 0
+    mov bl, 10          
+    div bl    
+    mov bl, ah
+    add al, 30h
+    mov offset msg4[2], al
+    add ah, 30h
+    mov offset msg4[3], ah         
+    mov dx,offset msg4
+    mov ah,9
+    int 21h
+.exit  
+end
